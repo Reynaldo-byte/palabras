98aadd2d18fc79426f82fec6437a5d1dafb30bbe
Reynaldo-byte <64993522+Reynaldo-byte@users.noreply.github.com>
2021-12-10 12:34:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function init() {
+	var random=Math.floor(Math.random() * (3));
+	switch (random) {
+		case 0:
+			window.location.replace("H.html");
+			break;
+		case 1:
+			window.location.replace("G.html");
+			break;
+		case 2:
+			window.location.replace("J.html");
+			break;
+		case 3:
+			window.location.replace("L.html");
+			break;
+		default:
+			break;
+
+	}
+	
+}
+
+function avanzar(ids) {
+	document.getElementById(ids).removeAttribute("hidden");
+	document.getElementById(ids-1).setAttribute("hidden","");
+	
+}
+
+function comprobar(resultado,ids) {
+	
+	if (resultado=='correcto') {
+		
+		document.getElementById("responder"+(ids-2)).removeAttribute("hidden")
+
+	}
+	if (resultado=='incorrecto') {
+
+		document.getElementById("responder"+(ids-2)).setAttribute("hidden","")
+	}
+}
+
+
